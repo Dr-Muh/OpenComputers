615fb53015849963645b5f5c9139b48fba3ca416
Dr-Muh <79505258+Dr-Muh@users.noreply.github.com>
2021-02-24 16:44:24 +0100

    Update clock.lua

--- clock.lua
+++ clock.lua
@@ -192,10 +192,17 @@ function drawPixel()
 end
 
 function show()
-  gpu.setBackground(colors.green, true)
+  
+  local w, h = gpu.getResolution()
+  gpu.fill(1, 1, w, h, " ") -- clears the screen
+  gpu.setForeground(0x000000)
+  gpu.setBackground(0xFFFFFF)
+--  gpu.fill(1, 1, w/2, h/2, "X") -- fill top left quarter of screen
+--  gpu.copy(1, 1, w/2, h/2, w/2, h/2)
 
+  gpu.fill(1, 1, 10, 10, colors.orange)
 
-
+--  gpu.setBackground(colors.green, true)
 end
 
 show()
