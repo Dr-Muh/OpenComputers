7dd09ee4895f158b7e4c69a44cb0e287a3de7197
Dr-Muh <79505258+Dr-Muh@users.noreply.github.com>
2021-03-16 19:35:03 +0100

    Add files via upload

---
+++ meManagement.lua
@@ -0,0 +1,11 @@
+local os = require("os")
+local thread = require("thread")
+local component = require("component")
+
+local adapter = component.proxy("41d6298c-f468-45b0-bf2f-47f678419db2")
+
+function main()
+
+end
+
+main()
