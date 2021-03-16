820c5227fde5afe026c8f62d131346f7cce85ca1
Dr-Muh <79505258+Dr-Muh@users.noreply.github.com>
2021-03-16 20:26:33 +0100

    Update meManagement.lua

--- meManagement.lua
+++ meManagement.lua
@@ -4,8 +4,19 @@ local component = require("component")
 
 local adapter = component.proxy("41d6298c-f468-45b0-bf2f-47f678419db2")
 
-function main()
+local meAutoCrafts = {{name = "calculationProcessor", goal = 16},
+                        {name = "engineeringProcessor", goal = 16},
+                        {name = "logicProcessor", goal = 16}}
 
+function testForCraft()
+    for itemToCraft in meAutoCrafts do
+        print(itemToCraft.name)
+        print(itemToCraft.goal)
+    end
+end
+
+function main()
+    testForCraft()
 end
 
 main()
