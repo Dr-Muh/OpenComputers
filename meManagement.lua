ff6f8739b56274443dd282d25c936a8726d27b20
Dr-Muh <79505258+Dr-Muh@users.noreply.github.com>
2021-03-17 10:24:45 +0100

    Update meManagement.lua

--- meManagement.lua
+++ meManagement.lua
@@ -4,19 +4,29 @@ local component = require("component")
 
 local adapter = component.proxy("41d6298c-f468-45b0-bf2f-47f678419db2")
 
-local meAutoCrafts = {{name = "calculationProcessor", goal = 16},
-                        {name = "engineeringProcessor", goal = 16},
-                        {name = "logicProcessor", goal = 16}}
+local meAutoCrafts = {"calculationProcessor", 16,
+                        "engineeringProcessor", 16,
+                        "logicProcessor", 16}
 
-function testForCraft()
-    for itemToCraft in meAutoCrafts do
-        print(itemToCraft.name)
-        print(itemToCraft.goal)
+
+
+function testCount(name, goal)
+    
+end
+
+function readList()
+    for i, v in ipairs(meAutoCrafts) do
+        if i%2 ~=0 then
+            name = meAutoCrafts[i]
+            goal = meAutoCrafts[i+1]
+            --print(meAutoCrafts[i] .. " " .. meAutoCrafts[i+1])
+            testCount(name, goal)
+        end
     end
 end
 
 function main()
-    testForCraft()
+    readList()
 end
 
 main()
