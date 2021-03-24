fb985903f0183f677ce41154afa610e3460731d9
Dr-Muh <79505258+Dr-Muh@users.noreply.github.com>
2021-03-24 20:44:44 +0100

    Update errorServer.lua

--- errorServer.lua
+++ errorServer.lua
@@ -2,22 +2,16 @@ local os = require("os")
 local term = require("term")
 local thread = require("thread")
 local event   = require("event")
+local sides = require("sides")
 local component = require("component")
 
 local redstone = component.proxy("975bae7d-2743-4992-817d-7eef4bbfdb3f")
 
 local modem = component.modem
 
-local gpu = component.gpu
-local screenAdress = "202f972d-2cf6-4b70-97d4-7fa09bdae43f"
-
 
 
 function main()
-    --term.clear()
-    gpu.bind(screenAdress)
-    --term.clear()
-
     mainThread = thread.create(function()
         testThread = thread.create(function()
             while true do
@@ -36,6 +30,9 @@ while true do
     local id, _, x, y = event.pullMultiple("interrupted")
     if id == "interrupted" then
       print("soft interrupt, closing")
+
+      redstone.setOutput({0, 0, 0, 0, 1, 0})
+
       os.exit()
       break
     end
