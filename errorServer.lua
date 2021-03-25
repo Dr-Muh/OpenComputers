39dd49ef0fe3bf41458fe6b65a422bfdff3f0291
Dr-Muh <79505258+Dr-Muh@users.noreply.github.com>
2021-03-25 17:30:03 +0100

    Update errorServer.lua

--- errorServer.lua
+++ errorServer.lua
@@ -5,22 +5,73 @@ local event   = require("event")
 local sides = require("sides")
 local component = require("component")
 
-local redstone = component.proxy("975bae7d-2743-4992-817d-7eef4bbfdb3f")
+local redstoneIO1 = component.proxy("975bae7d-2743-4992-817d-7eef4bbfdb3f")
 
 local modem = component.modem
 
+local errorStates = {}
+            errorStates["crafting failed"] = false
+            errorStates["mekanism generator fuel low"] = false
 
+
+
+function locateLamp(errorMessage, errorValue)
+    if errorMessage == "crafting failed" then switchLamp(redstoneIO1, sides.top, errorValue, false)
+    elseif errorMessage == "mekanism generator fuel low" then switchLamp(redstoneIO1, sides.right, errorValue, true) 
+    end
+end
+
+local importantThreadBool = false
+local importantThread = thread.create(function()
+    while true do
+        while importantThreadBool do
+            redstoneIO1.setOutput(sides.left, 15)
+            os.sleep(1)
+            redstoneIO1.setOutput(sides.left, 0)
+            os.sleep(1)
+        end
+        os.sleep(1)
+    end
+end)
+
+local importantCount = 0
+function switchLamp(redstoneIO, ioSide, errorValue, importantBoolean)
+    if errorValue then
+        redstoneIO.setOutput(ioSide, 15)
+    else
+        redstoneIO.setOutput(ioSide, 0)
+    end
+    if importantBoolean then
+        if errorValue then
+            importantCount = importantCount+1
+        else
+            importantCount = importantCount-1
+        end
+        if importantCount>0 then
+            importantThreadBool = true
+        else
+            importantThreadBool = false
+        end
+    end
+end
+
+function main_t()
+    local _, _, _, _, _, message, errorState = event.pull("modem_message")
+    thread.create(function()
+        main_t()
+    end)
+    if errorStates[message] ~= errorState then
+        locateLamp(message, errorState)
+    end
+    errorStates[message] = errorStates
+end
 
 function main()
+    modem.open(1)
+    redstoneIO1.setOutput({0, 0, 0, 0, 0, 0})
+
     mainThread = thread.create(function()
-        testThread = thread.create(function()
-            while true do
-                redstone.setOutput(sides.top, 15)
-                os.sleep(1)
-                redstone.setOutput(sides.top, 0)
-                os.sleep(1)
-            end
-        end)
+        main_t()
     end)
 end
 
@@ -31,7 +82,7 @@ while true do
     if id == "interrupted" then
       print("soft interrupt, closing")
 
-      redstone.setOutput({0, 0, 0, 0, 1, 0})
+      redstoneIO1.setOutput({0, 0, 0, 0, 1, 0})
 
       os.exit()
       break
