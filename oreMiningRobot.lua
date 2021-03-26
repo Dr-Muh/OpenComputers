2713a6905444792ff174dbdfbf0b1f0f2e89c9c9
Dr-Muh <79505258+Dr-Muh@users.noreply.github.com>
2021-03-26 20:38:10 +0100

    Create oreMiningRobot.lua

---
+++ oreMiningRobot.lua
@@ -0,0 +1,44 @@
+local os = require("os")
+local thread = require("thread")
+local event   = require("event")
+local robot = require("robot")
+local sides = require("sides")
+
+
+function checkInventory()
+    for i=1, 16 do
+        if robot.count(i) > 0 then
+            return false
+        end
+    end
+    return true
+end
+
+function dropItems()
+    for i=1, 16 do
+        robot.select(i)
+        robot.dropDown(64)
+    end
+end
+
+function main()
+    mainThread = thread.create(function()
+        while true do
+            robot.swing(sides.front)
+            if checkInventory() then
+
+            end
+        end
+    end)
+end
+
+main()
+
+while true do
+    local id, _, x, y = event.pullMultiple("interrupted")
+    if id == "interrupted" then
+      print("soft interrupt, closing")
+      os.exit()
+      break
+    end
+end
